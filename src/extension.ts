8bc4e70041b9703919dcda25bce086b75230c4d2
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2024-11-18 15:11:34 +0100

    feat: add command `Quarto Wizard: Clear Recently Installed Extensions`

--- src/extension.ts
+++ src/extension.ts
@@ -6,17 +6,22 @@ const QUARTO_WIZARD_LOG = vscode.window.createOutputChannel("Quarto Wizard");
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.commands.registerCommand("quartoExtensions.showOutput",
-			() => QUARTO_WIZARD_LOG.show()
-		)
-	);
-	// context.globalState.update(RECENTLY_INSTALLED_QUARTO_EXTENSIONS, []);
-	let disposable = vscode.commands.registerCommand(
-		"quartoExtensions.installExtension",
-		() => installQuartoExtensionCommand(context, QUARTO_WIZARD_LOG, RECENTLY_INSTALLED_QUARTO_EXTENSIONS)
+		vscode.commands.registerCommand("quartoWizard.showOutput", () => QUARTO_WIZARD_LOG.show())
 	);
 
-	context.subscriptions.push(disposable);
+	let clearRecentlyInstalledDisposable = vscode.commands.registerCommand(
+		"quartoWizard.clearRecentlyInstalled",
+		() => {
+			context.globalState.update(RECENTLY_INSTALLED_QUARTO_EXTENSIONS, []);
+			vscode.window.showInformationMessage("Recently installed Quarto extensions have been cleared.");
+		}
+	);
+	context.subscriptions.push(clearRecentlyInstalledDisposable);
+
+	let installExtensionDisposable = vscode.commands.registerCommand("quartoWizard.installExtension", () =>
+		installQuartoExtensionCommand(context, QUARTO_WIZARD_LOG, RECENTLY_INSTALLED_QUARTO_EXTENSIONS)
+	);
+	context.subscriptions.push(installExtensionDisposable);
 }
 
 export function deactivate() {}
