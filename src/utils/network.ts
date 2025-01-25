67f28900c04658146e5f3726cc5c235d4e5f87e6
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-01-25 21:57:52 +0100

    refactor(network): internal logging

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,13 +1,24 @@
-import * as dns from "dns";
+import * as vscode from "vscode";
+import { showLogsCommand } from "./log";
 
-export function checkInternetConnection(): Promise<boolean> {
-	return new Promise((resolve) => {
-		dns.lookup("github.com", (err) => {
-			if (err && err.code === "ENOTFOUND") {
-				resolve(false);
-			} else {
-				resolve(true);
-			}
-		});
-	});
+export async function checkInternetConnection(
+	url: string = "https://github.com/",
+	log: vscode.OutputChannel
+): Promise<boolean> {
+	try {
+		const response = await fetch(url);
+		if (response.ok) {
+			return true;
+		} else {
+			const message = `No internet connection. Please check your network settings. ${showLogsCommand()}.`;
+			log.appendLine(message);
+			vscode.window.showErrorMessage(message);
+			return false;
+		}
+	} catch (error) {
+		const message = `No internet connection. Please check your network settings. ${showLogsCommand()}.`;
+		log.appendLine(message);
+		vscode.window.showErrorMessage(message);
+		return false;
+	}
 }
