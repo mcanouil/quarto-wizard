7857fb2ad7063031e0e712d3435deed7e797cc3e
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-01-30 22:18:51 +0100

    fix: harmonise notification/log messages

--- src/utils/reprex.ts
+++ src/utils/reprex.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 import { QUARTO_WIZARD_LOG } from "../constants";
+import { showLogsCommand } from "./log";
 
 export async function newQuartoReprex(language: string, context: vscode.ExtensionContext) {
 	let templateFile = "";
@@ -17,18 +18,18 @@ export async function newQuartoReprex(language: string, context: vscode.Extensio
 			templateFile = "python.qmd";
 			break;
 		default:
-			const message = `Unsupported language: ${language}`;
+			const message = `Unsupported language: ${language}.`;
 			QUARTO_WIZARD_LOG.appendLine(message);
-			vscode.window.showErrorMessage(message);
+			vscode.window.showErrorMessage(`${message} ${showLogsCommand()}.`);
 			return;
 	}
 
 	const filePath = path.join(context.extensionPath, "assets", "templates", templateFile);
 	fs.readFile(filePath, "utf8", (err, data) => {
 		if (err) {
-			const message = `Failed to read the template file: ${err.message}`;
+			const message = `Failed to read the template file: ${err.message}.`;
 			QUARTO_WIZARD_LOG.appendLine(message);
-			vscode.window.showErrorMessage(message);
+			vscode.window.showErrorMessage(`${message} ${showLogsCommand()}.`);
 			return;
 		}
 
