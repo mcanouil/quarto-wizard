6e0c7c4f4b74d6deaf1b3ef6f77ce9240d047ecf
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-02-16 00:40:48 +0100

    chore: rm explorer view

--- src/ui/extensionsExplorer.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from "vscode";
-import * as path from "path";
-import * as fs from "fs";
-import { showLogsCommand } from "../utils/log";
-import { installQuartoExtensionSource } from "../utils/quarto";
-import { ExtensionDetails, getExtensionsDetails } from "../utils/extensionDetails";
