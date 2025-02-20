fefd238c3819c2e98a62b0fc9cade15d30490ff7
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-02-20 17:43:57 +0100

    chore: revert to 1-hour caching

--- src/constants.ts
+++ src/constants.ts
@@ -24,4 +24,4 @@ export const QW_EXTENSIONS_CACHE = "quarto_wizard_extensions";
 /**
  * Cache duration for the Quarto extensions JSON (default to 1 hour).
  */
-export const QW_EXTENSIONS_CACHE_TIME = 0 * 60 * 60 * 1000;
+export const QW_EXTENSIONS_CACHE_TIME = 1 * 60 * 60 * 1000;
