d4ec445682231e257571d5503ae1fea53f67d510
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-02-22 23:51:55 +0100

    feat: add modal box to confirm

--- src/utils/handleUri.ts
+++ src/utils/handleUri.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { showLogsCommand, logMessage } from "../utils/log";
 import { installQuartoExtensionSource } from "./quarto";
 
 /**
@@ -8,13 +9,26 @@ import { installQuartoExtensionSource } from "./quarto";
  *
  * vscode://mcanouil.quarto-wizard/install?repo=<owner>/<repository>
  */
-export function handleUri(uri: vscode.Uri) {
+export async function handleUri(uri: vscode.Uri) {
 	if (uri.path === "/install") {
 		const repo = new URLSearchParams(uri.query).get("repo");
 		if (!repo || !vscode.workspace.workspaceFolders) {
 			return;
 		}
 		const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+		const installWorkspace = await vscode.window.showInformationMessage(
+			`Do you confirm the installation of "${repo}" extension?`,
+			{ modal: true },
+			"Yes",
+			"No"
+		);
+
+		if (installWorkspace === "No") {
+			const message = "Operation cancelled by the user.";
+			logMessage(message, "info");
+			vscode.window.showInformationMessage(`${message} ${showLogsCommand()}.`);
+			return;
+		}
 		installQuartoExtensionSource(repo, workspaceFolder);
 	}
 }
