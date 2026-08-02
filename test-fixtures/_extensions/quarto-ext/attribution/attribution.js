970ebf6d6eb275f2a52817ff180fd51413a0f10f
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2026-08-02 21:40:49 +0200

    chore(test): reinstall quarto-attribution

--- test-fixtures/_extensions/quarto-ext/attribution/attribution.js
+++ test-fixtures/_extensions/quarto-ext/attribution/attribution.js
@@ -1,70 +1,75 @@
 /*****************************************************************
- ** Author: Roland Schmehl, r.schmehl@tudelft.nl
- **
- ** A plugin for displaying attribution texts sideways along the right
- ** edge of the viewport. When resizing the viewport or toggling full
- ** screen mode, the attribution text sticks persistently to the right
- ** edge of the viewport.
- **
- ** The dynamic scaling of the attribution text via CSS transform
- ** is adopted from the fullscreen plugin.
- **
- ** Version: 1.0
- **
- ** License: MIT license (see file LICENSE)
- **
- ******************************************************************/
+** Author: Roland Schmehl, r.schmehl@tudelft.nl
+**
+** A plugin for displaying attribution texts sideways along the right
+** edge of the viewport. When resizing the viewport or toggling full
+** screen mode, the attribution text sticks persistently to the right
+** edge of the viewport.
+**
+** The dynamic scaling of the attribution text via CSS transform
+** is adopted from the fullscreen plugin.
+**
+** Version: 1.0
+**
+** License: MIT license (see file LICENSE)
+**
+******************************************************************/
 
 window.RevealAttribution = window.RevealAttribution || {
-	id: "RevealAttribution",
-	init: function (deck) {
-		initAttribution(deck);
-	},
+  id: 'RevealAttribution',
+  init: function(deck) {
+      initAttribution(deck);
+  }
 };
 
-const initAttribution = function (Reveal) {
-	var ready = false;
-	var resize = false;
-	var scale = 1;
+const initAttribution = function(Reveal){
 
-	window.addEventListener("ready", function (event) {
-		var content;
+var ready = false;
+var resize = false;
+var scale = 1;
 
-		// Remove configured margin of the presentation
-		var attribution = document.getElementsByClassName("attribution");
-		var width = window.innerWidth;
-		var configuredWidth = Reveal.getConfig().width;
-		var configuredHeight = Reveal.getConfig().height;
+window.addEventListener( 'ready', function( event ) {
 
-		scale = 1 / (1 - Reveal.getConfig().margin);
+  var content;
 
-		for (var i = 0; i < attribution.length; i++) {
-			content = attribution[i].innerHTML;
-			attribution[i].style.width = configuredWidth + "px";
-			attribution[i].style.height = configuredHeight + "px";
-			attribution[i].innerHTML = "<span class='content'>" + content + "</span>";
-			attribution[i].style.transform = "translate( -50%, -50% ) scale( " + scale * 100 + "% ) rotate(-180deg)";
-		}
+  // Remove configured margin of the presentation
+  var attribution = document.getElementsByClassName("attribution");
+  var width = window.innerWidth;
+  var configuredWidth = Reveal.getConfig().width;
+  var configuredHeight = Reveal.getConfig().height;
 
-		// Scale with cover class to mimic backgroundSize cover
-		resizeCover();
-	});
+  scale = 1/(1-Reveal.getConfig().margin);
 
-	window.addEventListener("resize", resizeCover);
+  for (var i = 0; i < attribution.length; i++) {
+    content = attribution[i].innerHTML;
+    attribution[i].style.width = configuredWidth + "px";
+    attribution[i].style.height = configuredHeight + "px";
+    attribution[i].innerHTML = "<span class='content'>" + content + "</span>";
+    attribution[i].style.transform = 'translate( -50%, -50% ) scale( ' + scale*100 + '% ) rotate(-180deg)';
+  }
 
-	function resizeCover() {
-		// Scale to mimic backgroundSize cover
-		var attribution = document.getElementsByClassName("attribution");
-		var xScale = window.innerWidth / Reveal.getConfig().width;
-		var yScale = window.innerHeight / Reveal.getConfig().height;
-		var s = 1;
+  // Scale with cover class to mimic backgroundSize cover
+  resizeCover();
 
-		if (xScale > yScale) {
-			// The div fits perfectly in x axis, stretched in y
-			s = xScale / yScale;
-		}
-		for (var i = 0; i < attribution.length; i++) {
-			attribution[i].style.transform = "translate( -50%, -50% ) scale( " + s * scale * 100 + "% ) rotate(-180deg)";
-		}
-	}
+});
+
+window.addEventListener( 'resize', resizeCover );
+
+function resizeCover() {
+
+  // Scale to mimic backgroundSize cover
+  var attribution = document.getElementsByClassName("attribution");
+  var xScale = window.innerWidth / Reveal.getConfig().width;
+  var yScale = window.innerHeight / Reveal.getConfig().height;
+  var s = 1;
+
+  if (xScale > yScale) {
+      // The div fits perfectly in x axis, stretched in y
+      s = xScale/yScale;
+  }
+  for (var i = 0; i < attribution.length; i++) {
+    attribution[i].style.transform = 'translate( -50%, -50% ) scale( ' + s*scale*100 + '% ) rotate(-180deg)';
+  }
+}
+
 };
